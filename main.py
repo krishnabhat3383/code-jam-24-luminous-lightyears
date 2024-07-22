78ccddae0637cbe52b48f081141dc68884e6dd14
hazyfossa <148675205+hazyfossa@users.noreply.github.com>
2024-07-22 21:19:13 +0500

    Improve how we're starting our app (#2)
    * Handle if token isn't provided
    
    * Add logging
    
    * make python-dotenv optional
    
    * Add developer mode
    
    * Add periods to docstrings

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
+import logging
 from os import getenv
+from sys import argv, exit
 
-from dotenv import load_dotenv
 from interactions import (
     Client,
     Intents,
     listen,
 )
 
-load_dotenv()
-bot = Client(intents=Intents.DEFAULT)
+log = logging.getLogger("defcon-internal")
+bot = Client(intents=Intents.DEFAULT, logging_level=logging.WARNING)
 
 
 @listen()
 async def on_ready() -> None:
-    """Print "Ready", when the bot is accepting commands."""
-    print("Ready")
+    """Notify that the bot is started."""
+    log.info("Bot started.")
 
 
-bot.load_extension("src.game_start")
-bot.start(getenv("BOT_TOKEN"))
+def get_token() -> str:
+    """Try to read bot's token from environment or .env."""
+
+    try:
+        from dotenv import load_dotenv
+
+        load_dotenv()
+        no_dotenv = False
+    except ImportError:
+        no_dotenv = True
+
+    token = getenv("DEFCON_BOT_TOKEN")
+
+    if token is None:
+        log.error("Token not found.\nPlease specify discord bot token via environment variable 'DEFCON_BOT_TOKEN'.")
+
+        if no_dotenv:
+            log.info("To read token from .env, install 'python-dotenv'")
+
+        exit()
+
+    return token
+
+
+def get_developer_mode() -> bool:
+    """Get if the bot is running in dev mode."""
+    try:
+        return argv[1] == "--dev"
+    except IndexError:
+        return False
+
+
+DEV = get_developer_mode()
+
+if __name__ == "__main__":
+    if DEV:
+        bot.load_extension("interactions.ext.jurigged")
+
+    bot.load_extension("src.game_start")
+    bot.start(token=get_token())
