be1bae9763368115d68e6a5c464eb3c0512cc404
hazyfossa <hazyfossa@gmail.com>
2024-07-22 19:53:20 +0500

    make python-dotenv optional

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import logging
 from os import getenv
 from sys import exit
 
-from dotenv import load_dotenv
 from interactions import (
     Client,
     Intents,
@@ -14,16 +13,33 @@ bot = Client(intents=Intents.DEFAULT, logging_level=logging.WARNING)
 
 
 @listen()
-async def on_ready() -> None:
+async def on_ready() -> None:  # noqa: D103
     log.info("Bot started.")
 
 
-load_dotenv()
-token = getenv("DEFCON_BOT_TOKEN")
+def get_token() -> str:
+    """Try to read bot's token from environment or .env"""
 
-if token is None:
-    log.error("Token not found.\nPlease specify discord bot token via environment variable 'DEFCON_BOT_TOKEN'.")
-    exit()
+    try:
+        from dotenv import load_dotenv
+
+        load_dotenv()
+        no_dotenv = False
+    except ImportError:
+        no_dotenv = True
+
+    token = getenv("DEFCON_BOT_TOKEN")
+
+    if token is None:
+        log.error("Token not found.\nPlease specify discord bot token via environment variable 'DEFCON_BOT_TOKEN'.")
+
+        if no_dotenv:
+            log.info("To read token from .env, install 'python-dotenv'")
+
+        exit()
+
+    return token
+
 
 bot.load_extension("src.game_start")
-bot.start(token)
+bot.start(token=get_token())
