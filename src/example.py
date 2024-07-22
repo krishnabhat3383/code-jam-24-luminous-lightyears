db2cda773e7d79a36518be64993ec381d88e8392
hazyfossa <hazyfossa@gmail.com>
2024-07-22 20:26:00 +0500

    Templating draft

---
+++ src/example.py
@@ -0,0 +1,22 @@
+from models import Actor, StageGroup, Template
+
+# fmt: off
+Actor("John the Farmer", "url_here",[
+    StageGroup(1, [
+        Template(
+            "Hello, leader of {nation_name}. Can you spare some money for our new farming equipment?",
+            choices={
+                "Yeah, sure": {"money": -10, "loyalty": +5},
+                "Nope": {"loyalty": +5},
+            },
+        ),
+        Template(
+            "Things are great, we managed to grow even more crops than expected. Thank you for your help, leader! \n Here, have this small gift from our community", 
+            choices = {"Thanks!": {"money": +15}},
+            condition = lambda state: state.loyalty > 70 and state.money < 300,
+        ),
+    ]),
+    StageGroup([2, 3], [
+        Template("Etc, you get the idea"),
+    ]),
+])
