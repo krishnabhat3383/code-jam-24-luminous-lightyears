9ae9f3cdeb6738b10b4bbc8750d98ba4bb452cd1
Sapient44 <doineedaname18@gmail.com>
2024-07-23 23:07:10 +0530

    Remove player

--- src/game.py
+++ src/game.py
@@ -4,6 +4,9 @@ from models import State
 
 GameID = str
 
+# Tasks to be achieved here 
+# 1) Create the game flow, and when the template objects would be called 
+# 2) Make an internal game, so we can start testing
 
 class Game:
     def __init__(self, id: GameID) -> None:
@@ -12,3 +15,6 @@ class Game:
 
     async def add_player(self, user: User, nation_name: str) -> None:
         self.players[user.id] = State(nation_name)
+
+    async def remove_player(self, user:User) -> None:
+        del self.players[user.id]
