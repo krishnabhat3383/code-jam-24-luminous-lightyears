a8f6e7b742bd6be7a2ba9208d1af8c03e4f53f56
hazyfossa <hazyfossa@gmail.com>
2024-07-23 20:08:12 +0500

    Basic game management draft

---
+++ src/game.py
@@ -0,0 +1,14 @@
+from interactions import User
+
+from models import State
+
+GameID = str
+
+
+class Game:
+    def __init__(self, id: GameID) -> None:
+        self.id = id
+        self.players: dict[str, State] = {}
+
+    async def add_player(self, user: User, nation_name: str) -> None:
+        self.players[user.id] = State(nation_name)
--- src/game_start.py
+++ src/game_start.py
@@ -8,54 +8,58 @@ from interactions import (
     SlashContext,
     component_callback,
     slash_command,
+    slash_option,
 )
 
-users_ids: list[int] = []  # Global Var for the players in the game
+from game import Game, GameID
+
+# Game = dict[Member, State]
 
 
-class GameInitiation(Extension):
+class GameManager:
+    def __init__(self) -> None:
+        self.store: dict[GameID, Game] = {}
+
+    def generate_game_id() -> GameID:
+        return "alpha-bravo"  # Generate randomly
+
+    def create_game(self) -> Game:
+        game_id = self.generate_game_id()
+        game = Game(game_id)
+        self.store[game_id] = game
+
+        return Game
+
+    def query_game(self, game_id: GameID) -> Game:
+        return self.store.get(game_id, None)
+
+
+class GameInitializon(Extension):
     """Control the extension entry point."""
 
-    @slash_command(name="start_defcord", description="Welcome to the simulation called DEFCORD")
-    async def my_command_function(self, ctx: SlashContext) -> None:
-        """Start the game of DEFCORD."""
-        # Here would need to check are there any other instances of the game
-        components = ActionRow(
-            Button(
-                style=ButtonStyle.GREEN,
-                label="Join",
-                custom_id="join_game",
-            ),
-            Button(
-                style=ButtonStyle.RED,
-                label="Leave",
-                custom_id="leave_game",
-            ),
-        )
+    def __init__(self, _) -> None:
+        self.manager = GameManager()
+
+    @slash_command(name="defcord_create_game", description="Welcome to the simulation called DEFCORD")
+    async def create(self, ctx: SlashContext) -> None:
+        game = self.manager.create_game()  # Add the first player here
+
         embed = Embed(
-            title="Starting DEFCORD",
-            description=f"The tactical game of DEFCORD has been initiated by <@{ctx.user.id}>",
+            title="New game started!",
+            description=f"Your invite: {game.id}",
             color=(255, 0, 0),
         )
-        await ctx.send(embed=embed, components=components)
 
-    @component_callback("join_game")
-    async def join_callback(self, ctx: ComponentContext) -> None:
-        """Control player joining the game."""
-        # Need to make this channel wise
-        user = ctx.user
-        if user.id not in users_ids:
-            await ctx.send(f"Player <@{user.id}> has joined")
-            users_ids.append(user.id)
-        else:
-            await ctx.send("You are already part of the game", ephemeral=True)
+        await ctx.send(embed=embed)
 
-    @component_callback("leave_game")
-    async def leave_callback(self, ctx: ComponentContext) -> None:
-        """Control players leaving the game."""
-        user = ctx.user
-        if user.id in users_ids:
-            await ctx.send(f"Player <@{user.id}> has left")
-            users_ids.remove(user.id)
-        else:
-            await ctx.send("You are not part of the game", ephemeral=True)
+    @slash_command(name="start_defcord", description="Welcome to the simulation called DEFCORD")
+    @slash_option("invite", "The invite code for the game", required=True)
+    async def join(self, ctx: SlashContext, invite: str) -> None:
+        """Start the game of DEFCORD."""
+
+        game = self.manager.query_game(invite)
+
+        if game is None:
+            raise NotImplementedError
+
+        await game.add_player(ctx.user, "")  # Ask nation name here
