7794f78892ffe84cc4fd00f93c43f83277e6a96c
Sapient44 <doineedaname18@gmail.com>
2024-07-23 17:15:45 +0530

    Add embedding of Template

--- src/models.py
+++ src/models.py
@@ -2,13 +2,15 @@ from collections.abc import Callable
 from typing import Any, Literal, get_args
 
 from attrs import asdict, define, field, frozen
-from interactions.models.discord.embed import Embed
+from interactions import ActionRow, Button, ButtonStyle, Embed
 
 Stage = Literal[1, 2, 3]  # Adjustable
 
 
 @define
 class State:
+    """State the current state of the player."""
+
     nation_name: str
     stage: Stage = 1
 
@@ -28,9 +30,13 @@ Condition = Callable[[State], bool] | None
 
 @frozen
 class Template:
+    """Make a template for the messages to be served."""
+
     @staticmethod
     def convert_condition(condition: Condition | None) -> Condition:
+        """Convert based on the condition."""
         def always_true(_: State) -> True:
+            """True/"""
             return True
 
         if condition is None:
@@ -38,16 +44,32 @@ class Template:
 
         return condition
 
-    string: str
+    text: str
     choices: dict[None, Consequence]
     condition: Condition = field(converter=convert_condition)
 
     def format(self, state: State) -> str:
-        return self.string.format(asdict(state))
+        """Format the text."""
+        return self.text.format(asdict(state))
 
-    def to_embed(self, state: State) -> Embed:
-        raise NotImplementedError
-
+    def to_embed(self, state: State) -> list[Embed, ActionRow]:
+        """Return embed and action row for the UI purpose."""
+        buttons : list[Button] = []
+        for id, choice in enumerate(self.choices):
+            button = Button(
+                label = f"{list(choice.keys())[0]}",
+                ButtonStyle = ButtonStyle.BLURPLE,
+                custom_id = f"Choice {id}",
+            )
+            buttons.append(button)
+        action_row = ActionRow(*buttons)
+        embed = Embed(
+            title = state.nation_name,
+            description = self.text,
+            color=(0, 0, 255),
+            # Can we access Actor here in this class? like this actor is saying this
+        )
+        return [embed, action_row]
 
 # StageSpec = Stage | tuple[Stage] | Literal["all"]
 TotalStages = get_args(Stage)
@@ -55,6 +77,8 @@ TotalStages = get_args(Stage)
 
 @frozen
 class StageGroup:
+    """State Stage Group."""
+
     # @staticmethod
     # def convert_stage(stage: StageSpec) -> Stage | tuple[Stage]:
     #     if stage == "all":
@@ -70,6 +94,8 @@ class StageGroup:
 
 @frozen
 class Actor:
+    """State Actor."""
+
     name: str
     picture: str  # we'll need to serve these as static content probably
     templates: list[StageGroup]
