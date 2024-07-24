42a9d8debaaa3a758b6a7cb6da5bb8ee0d4d6245
DhanvantG <dhanvantg@gmail.com>
2024-07-24 16:10:58 +0530

    Sample

--- src/example.py
+++ src/example.py
@@ -6,18 +6,29 @@ Actor("John the Farmer", "url_here",[
         Template(
             "Hello, leader of {nation_name}. Can you spare some money for our new farming equipment?",
             choices={
-                "Yeah, sure": {"money": -10, "loyalty": +5},
+                "Sure": {"money": -10, "loyalty": +5},
                 "Nope": {"loyalty": -5},
-            },
+            }
         ),
         Template(
-            """Things are great, we managed to grow even more crops than expected.
-                Thank you for your help, leader! \n Here, have this small gift from our community""",
+            "Things are great, we managed to grow even more crops than expected.\
+                Thank you for your help, leader! \n Here, have this small gift from our community",
             choices = {"Thanks!": {"money": +15}},
             condition = lambda state: state.loyalty>70 and state.money<300,
         ),
     ]),
-    StageGroup([2, 3], [
-        Template("Etc, you get the idea"),
+    StageGroup(2, [
+        Template("O Great leader of {nation_name}! Please provide money to upgrade our equipment",
+                 choices = {
+                     "Sure": {"money": -20, "loyalty": +5},
+                     "Nope": {"loyalty": -5}
+                 }
+            ),
+        Template(
+            "Things are great, we managed to grow even more crops than expected.\
+                Thank you for your help, leader! \n Here, have this small gift from our community",
+            choices = {"Thanks!": {"money": +30}},
+            condition = lambda state: state.loyalty>75 and state.money<800,
+        ),
     ]),
 ])
