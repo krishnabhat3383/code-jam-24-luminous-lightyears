acd9b71b5a176e9e1809965649f79bde91210683
hazyfossa <hazyfossa@gmail.com>
2024-07-25 21:05:50 +0500

    move code to characters __init__.py

---
+++ src/characters/__init__.py
@@ -0,0 +1,21 @@
+from importlib import import_module
+from logging import getLogger
+from pathlib import Path
+
+from templating import Actor
+from weighted_random import WeightedList
+
+log = getLogger("character-init")
+
+characters: WeightedList[Actor] = WeightedList()
+
+for file in Path(__file__).parent.glob("*.chr.py"):
+    if not file.is_file() or file.name == __file__:
+        continue
+
+    try:
+        characters.append(character := import_module(file.name).character)
+        log.debug("Loaded {character.name} character from {file}.")
+    except Exception:
+        log.exception("Character file {file} is invalid. Skipping.")
+        continue
