ca6e8439d1c648ed597ffcf38e14ff6594b9e908
hazyfossa <hazyfossa@gmail.com>
2024-07-25 20:46:49 +0500

    add append to WeightedList

--- src/weighted_random.py
+++ src/weighted_random.py
@@ -11,9 +11,16 @@ T = TypeVar("T", bound=SupportsWeight)
 
 
 class WeightedList(Generic[T]):
-    def __init__(self, values: list[T]) -> None:
-        self.values = values
-        self.weights = [value.weight for value in self.values]
+    def __init__(self, values: list[T] | None = None) -> None:
+        if values is not None:
+            self.values = values
+            self.weights = [value.weight for value in values]
+        else:
+            self.values = []
+
+    def append(self, value: T) -> None:
+        self.values.append(value)
+        self.weights.append(value.weight)
 
     def get_random(self) -> T:
         return random.choices(self.values, weights=self.weights, k=1)[0]  # noqa: S311 Not for cryptographic purposes
