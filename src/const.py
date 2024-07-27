f5e6e2aa9956825a55fe34fd0c274809e9d95d27
Sapient44 <doineedaname18@gmail.com>
2024-07-27 14:25:01 +0530

    Define some coloured accents

--- src/const.py
+++ src/const.py
@@ -1,3 +1,5 @@
 from interactions import Embed
 
-error_embed = Embed()
+error_embed = Embed(color=(255, 58, 51))
+message_embed = Embed(color=(124, 199, 242))
+system_message_embed = Embed(color=(88, 245, 149))
