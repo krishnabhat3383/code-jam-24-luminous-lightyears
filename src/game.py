f9aae25e9886b7f5f918665685bd0e2558d26e90
Maheshkumar <callmeelprofessor@gmail.com>
2024-07-27 13:35:23 +0530

    Add start logic and fix few bugs

--- src/game.py
+++ src/game.py
@@ -1,12 +1,11 @@
 import asyncio
-
-
-from attrs import define
+import logging
 import random
 from datetime import datetime, timedelta
 from typing import TYPE_CHECKING, Annotated
 
 from interactions import SlashContext
+
 from src.characters import all_characters
 from src.const import error_embed
 from src.player import Player
@@ -15,35 +14,35 @@ from src.templating import total_stages
 if TYPE_CHECKING:
     from src.templating import Stage
 
-class Player:
-    """Define and register the data related to the player."""
-
-    def __init__(self, ctx: SlashContext, game: "Game") -> None:
-        self.ctx = ctx
-        self.state: PlayerState = None  # type: ignore TODO: properly type that state isn't none after register
-        self.game = game
 
 GameID = str
+logger = logging.getLogger(__name__)
 
 
 class Game:
     """Initialize a Game and it's behaviours."""
 
-    def __init__(self, id: GameID) -> None:
+    def __init__(self, id: GameID, required_no_of_players: int) -> None:
         self.id = id
+        self.required_no_of_players: int = required_no_of_players
         self.players: dict[Annotated[int, "discord id"], Player] = {}
         self.stage: Stage = 1
         self.max_time: float = random.uniform(12.5, 16)
+        self.started: bool = False
+        self.creator: int | None = None
+        self.stop_flag: bool = False
 
         self.cumm_percent_time_per_stage: list[float] = [0.25, 0.6, 1]
         # Percentage of the time spent in the game when the next stage of the time begins (max value 1 = 100%)
         self.values_to_check: list[str] = ["loyalty", "money", "security", "world_opinion"]
 
-    async def add_player(self, ctx: SlashContext) -> None:
+    async def add_player(self, ctx: SlashContext, cmd: str = "create") -> None:
+        """Add a player to the game."""
+        if cmd == "create":
+            self.creator = ctx.user.id
         player = Player(ctx, self)
         await player.register()
         self.players[ctx.user.id] = player
-
 
     async def remove_player(self, ctx: SlashContext) -> None:
         """Remove player from the game."""
@@ -51,8 +50,12 @@ class Game:
         try:
             del self.players[player_to_delete]
         except KeyError:
-            raise NotImplementedError
+            raise NotImplementedError from KeyError
         # Need to pass this error to the user, that you are in no game
+
+    def stop(self) -> None:
+        """Set the stop flag."""
+        self.stop_flag = True
 
     async def loop(self) -> None:
         """Define the main loop of the game."""
@@ -61,6 +64,9 @@ class Game:
         players = self.players.values()
 
         while True:
+            if self.stop_flag:
+                break
+
             game_time: float = (datetime.now() - self.start_time) / timedelta(minutes=1)
             if (game_time > self.cumm_percent_time_per_stage[self.stage - 1] * self.max_time) and (
                 game_time < self.max_time
@@ -70,13 +76,22 @@ class Game:
                 ]  # This isn't the best, but it won't go out of bounds and doesn't break typing
 
             try:
-                await asyncio.gather(*[self.tick(player) for player in players], return_exceptions=True)
-            except Exception:  # noqa: BLE001 this is intentional
+                response = await asyncio.gather(*[self.tick(player) for player in players], return_exceptions=True)
+
+                for res in response:
+                    if isinstance(res, Exception):
+                        logger.error(res)
+            except Exception:
+                logger.exception("Error occurred in game loop")
+
                 for player in players:
                     await player.ctx.send(embed=error_embed)
 
     async def tick(self, player: Player) -> None:
         """Define the activities done in every game tick."""
+        if self.stop_flag:
+            return
+
         character = all_characters.get_random(player.state)
         # The sleep times are subject to change, based on how the actual gameplay feels
         # The randomness gives a variability between the values mentioned in the brackets
--- src/game_interaction.py
+++ src/game_interaction.py
@@ -18,27 +18,44 @@ if TYPE_CHECKING:
 
 
 class GameFactory:
+    """Game creator factory class."""
+
     def __init__(self) -> None:
         self.games: dict[GameID, Game] = {}
         self.players: dict[int, Game] = {}
 
     def generate_game_id(self) -> GameID:
+        """Generate a random id for the game."""
         while True:
-            game_id = "".join(random.choice(ascii_uppercase + digits) for i in range(12))  # noqa: S311 This isn't for crypto purposes
+            game_id = "".join(random.choice(ascii_uppercase + digits) for i in range(6))
 
             if game_id in self.games:
                 continue
 
             return game_id
 
-    def create_game(self) -> Game:
+    def create_game(self, required_no_of_players: int) -> Game:
+        """Create a game with the required details."""
         game_id = self.generate_game_id()
-        game = Game(game_id)
+        game = Game(game_id, required_no_of_players)
         self.games[game_id] = game
 
         return game
 
+    def add_player(self, player_id: int, game: Game) -> None:
+        """Map a player with a game."""
+        self.players[player_id] = game
+
+    def remove_player(self, player_id: int) -> None:
+        """Remove a player game mapping."""
+        del self.players[player_id]
+
+    def remove_game(self, game_id: int) -> None:
+        """Remove a game from game id mapping."""
+        del self.games[game_id]
+
     def query_game(self, game_id: GameID | None = None, player_id: int | None = None) -> Game | None:
+        """Return the game based on the query details."""
         if game_id:
             return self.games.get(game_id, None)
         if player_id:
@@ -54,16 +71,46 @@ class GameInteraction(Extension):
     def __init__(self, _: "Client") -> None:
         self.game_factory = GameFactory()
 
+    async def send_player_join_notification(self, game: Game, ctx: SlashContext) -> None:
+        """Send a notification to all the players that a player has joined the game."""
+        for player in game.players.values():
+            remaining_players_count = game.required_no_of_players - len(game.players)
+            if remaining_players_count:
+                count_message = f"{remaining_players_count} yet to join."
+            else:
+                count_message = "All aboard. The game creator can start the game now."
+
+            await player.ctx.send(
+                f"<@{ctx.user.id}> joined the game.\n{count_message}",
+                ephemeral=True,
+            )
+
     @slash_command(name="defcord", description="Interact with defcord.")
     async def defcord(self, ctx: SlashContext) -> None:
         """Make the subcommand work, since it requires a function to latch off of."""
 
     @defcord.subcommand(sub_cmd_name="create", sub_cmd_description="Create a game of Defcord")
-    async def create(self, ctx: SlashContext) -> None:
+    @slash_option(
+        "required_no_of_players",
+        "Number of players required for the game",
+        required=True,
+        opt_type=OptionType.INTEGER,
+        min_value=1,
+        max_value=10,
+    )
+    async def create(self, ctx: SlashContext, required_no_of_players: int = 5) -> None:
         """Create a game of DEFCORD."""
-        game = self.game_factory.create_game()  # Add the first player here
+        existing_game = self.game_factory.query_game(player_id=ctx.user.id)
+        if existing_game:
+            await ctx.send(
+                f"<@{ctx.user.id}> You are already part of the game with invite {existing_game.id}",
+                ephemeral=True,
+            )
+            return
 
-        await game.add_player(ctx)
+        game = self.game_factory.create_game(required_no_of_players)
+        self.game_factory.add_player(ctx.user.id, game)
+        await game.add_player(ctx, cmd="create")
 
         embed = Embed(
             title="New game started!",
@@ -72,6 +119,7 @@ class GameInteraction(Extension):
         )
 
         await ctx.send(embed=embed)
+        await self.send_player_join_notification(game, ctx)
 
     @defcord.subcommand(sub_cmd_name="join", sub_cmd_description="Join a game of Defcord")
     @slash_option("invite", "The invite code for the game", required=True, opt_type=OptionType.STRING)
@@ -80,20 +128,77 @@ class GameInteraction(Extension):
         game = self.game_factory.query_game(game_id=invite)
 
         if game is None:
-            raise NotImplementedError
+            await ctx.send(f"<@{ctx.user.id}> Invite({invite}) is invalid", ephemeral=True)
+            return
 
-        await game.add_player(ctx)
+        if ctx.user.id in game.players:
+            await ctx.send(f"<@{ctx.user.id}> You are already part of the game with {invite=}", ephemeral=True)
+            return
+
+        if game.required_no_of_players == len(game.players):
+            await ctx.send(f"<@{ctx.user.id}> Game is already full {invite=}", ephemeral=True)
+            return
+
+        if game.started:
+            await ctx.send(f"<@{ctx.user.id}> Game already started", ephemeral=True)
+            return
+
+        self.game_factory.add_player(ctx.user.id, game)
+        await game.add_player(ctx, cmd="join")
+        await self.send_player_join_notification(game, ctx)
 
     @defcord.subcommand(sub_cmd_name="leave", sub_cmd_description="Leave a game of Defcord")
     async def leave(self, ctx: SlashContext) -> None:
-        """Leave the current game of defcord"""
+        """Leave the current game of defcord."""
         game = self.game_factory.query_game(player_id=ctx.user.id)
 
         if game is None:
-            raise NotImplementedError
-        else:
-            embed = Embed(title= "A Player left the game", 
-                          description=f"<@{ctx.user.id}> has left the game", color=(255, 0, 0))
-            await game.remove_player(ctx)
-            for player in game.players.values():
-                await player.ctx.send(embed=embed, ephemeral=True)
+            await ctx.send(f"<@{ctx.user.id}> You are not part of any game", ephemeral=True)
+            return
+
+        # if game.creator == ctx.user.id: # TODO: check this validity - needed of not
+        #     await ctx.send(f"<@{ctx.user.id}> Game creator cannot leave the game", ephemeral=True)  # noqa: ERA001
+        #     return  # noqa: ERA001
+
+        self.game_factory.remove_player(ctx.user.id)
+        await game.remove_player(ctx)
+
+        embed = Embed(
+            title="A Player left the game",
+            description=f"<@{ctx.user.id}> has left the game ({len(game.players)} players left).",
+            color=(255, 0, 0),
+        )
+        for player in game.players.values():
+            await player.ctx.send(embed=embed, ephemeral=True)
+
+        await ctx.send(embed=embed, ephemeral=True)
+
+        if len(game.players) == 0:
+            await ctx.send("Game Over! You are the only one survivor. Everyone quit!", ephemeral=True)
+            game.stop()
+            self.game_factory.remove_game(game.id)
+
+    @defcord.subcommand(sub_cmd_name="start", sub_cmd_description="Start a game of DEFCORD.")
+    async def start(self, ctx: SlashContext) -> None:
+        """Start and runs the Defcord game loop."""
+        game = self.game_factory.query_game(player_id=ctx.user.id)
+
+        if game is None:
+            await ctx.send(f"<@{ctx.user.id}> You are not part of any game", ephemeral=True)
+            return
+
+        if game.creator != ctx.user.id:
+            await ctx.send(f"<@{ctx.user.id}> Only game creator can start it", ephemeral=True)
+            return
+
+        if game.started:
+            await ctx.send(f"<@{ctx.user.id}> Game already started", ephemeral=True)
+            return
+
+        if game.required_no_of_players != len(game.players):
+            await ctx.send(f"<@{ctx.user.id}> Cannot start the game until all the players join", ephemeral=True)
+            return
+
+        game.started = True
+        await ctx.send(f"<@{ctx.user.id}> Game started", ephemeral=True)
+        await game.loop()
